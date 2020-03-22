42da115665226cc210cb88facf959458098808c2
Swapnil Darmora <darmora2@illinois.edu>
2020-03-22 22:33:56 +0530

    Adding changes for titlecounter

--- TitleCountReducer.py
+++ TitleCountReducer.py
@@ -8,7 +8,7 @@ wordcount = dict()
 
 # input comes from STDIN
 for line in sys.stdin:
-    getter = operator.itemgetter(0,1)
+    getter = itemgetter(0,1)
     getterval = getter(line)
     if getterval[0] in wordcount:
         wordcount[getterval[0]] = wordcount[getterval[0]] +11

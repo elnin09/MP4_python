0c613eaaae2ed56ce4c4561558510a086c76d550
Swapnil Darmora <darmora2@illinois.edu>
2020-03-23 00:42:36 +0530

    for her

--- TopTitlesMapper.py
+++ TopTitlesMapper.py
@@ -2,14 +2,8 @@
 import sys
 
 
-# TODO
-
 
 
 for line in sys.stdin:
        mapoutput = line.split('\t', 1)
-       print('%s\t%s' % (1,mapoutput)
-
-
-#TODO
-# print('%s\t%s' % (  ,  )) pass this output to reducer
+       print('%s\t%s' % (1,mapoutput))

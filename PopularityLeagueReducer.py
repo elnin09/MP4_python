47b167bc041a0734e9f64b4b5f5f6df4f7c250b3
Swapnil Darmora <darmora2@illinois.edu>
2020-03-24 04:20:12 +0530

    checking

--- PopularityLeagueReducer.py
+++ PopularityLeagueReducer.py
@@ -16,13 +16,23 @@ for line in sys.stdin:
 
 ret = sorted(keydata.items(), key=lambda kv: kv[1] )
 
+
+size = len(ret)
+i=0
+ans = list()
+while(i<size):
+    ans.append(ret[size-i-1])
+    #print ret[size-i-1]
+    i+=1
+
+
 counter=0
-for item in ret:
-    item[1]=counter
-    counter=counter+1
+for counter in range(1,len(ans)):
+     ans[counter-1][1] = counter-1
+     counter=counter+1
 
 
-print(ret)
+print(ans)
 
  
 

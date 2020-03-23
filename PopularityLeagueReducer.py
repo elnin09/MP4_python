a2b34e03bd190738e47ca13c01b21fe52f3ed28b
Swapnil Darmora <darmora2@illinois.edu>
2020-03-24 04:00:54 +0530

    checking

--- PopularityLeagueReducer.py
+++ PopularityLeagueReducer.py
@@ -8,16 +8,10 @@ keydata = dict()
 for line in sys.stdin:
     mapdata = (line.rstrip('\n')).split('\t',2)
     key,value = int(mapdata[1]),int(mapdata[2])
+    keydata[key]=value
 
 
-    if key in keydata.keys():
-        keydata[key]=keydata[key]+1
-        #if value not in keydata.keys():
-            #keydata[value]=0
-    else:
-        keydata[key]=1
-        if value not in keydata.keys():
-            keydata[value]=0
+ 
         
 
 ret = sorted(keydata.items(), key=lambda kv: kv[1] )
